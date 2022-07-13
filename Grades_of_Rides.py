cb15fc4b96f49b2587d3325eb101a22bfb9224cd
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-07-13 11:17:52 +0530

    h,s,sf=map(int,input().split()) if h>50 and s>60 and sf>100:     print("10") elif h>50 and s>60:     print("9") elif s>60 and sf>100:     print("8") elif h>50 and sf>100:     print("7") elif h>50 or s>60 or sf>100:     print("6") else:     print("5")

---
+++ Grades_of_Rides.py
@@ -0,0 +1,13 @@
+h,s,sf=map(int,input().split())
+if h>50 and s>60 and sf>100:
+    print("10")
+elif h>50 and s>60:
+    print("9")
+elif s>60 and sf>100:
+    print("8")
+elif h>50 and sf>100:
+    print("7")
+elif h>50 or s>60 or sf>100:
+    print("6")
+else:
+    print("5")
