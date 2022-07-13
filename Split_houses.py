51305abaaaa7dcfca588e05af4aa4790253f9876
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-07-13 09:57:06 +0530

    a=int(input()) s=input() c=0 for i in s:     if i=='.':         c+=1 if c>a//2:     print("YES")     for i in s:         if i=='.':             print("B",end="")         else:             print(i,end='') else:     print("NO")

---
+++ Split_houses.py
@@ -0,0 +1,15 @@
+a=int(input())
+s=input()
+c=0
+for i in s:
+    if i=='.':
+        c+=1
+if c>a//2:
+    print("YES")
+    for i in s:
+        if i=='.':
+            print("B",end="")
+        else:
+            print(i,end='')
+else:
+    print("NO")
