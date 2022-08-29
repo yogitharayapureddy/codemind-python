64d95c8b1ca78aeb8be43b636459959702ac5da4
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-08-29 20:46:18 +0530

    a,b=map(int,input().split()) a=min(a,b) i=a while i:     if a%i==0 and b%i==0:         print(i)         break     i-=1

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,11 +1,8 @@
-def gcd(x,y):
-    if(x>y):
-        small=y
-    else:
-        small=x
-    for i in range(1,small+1):
-        if((x%i==0) and(y%i==0)):
-            gcd=i
-    return gcd
 a,b=map(int,input().split())
-print(gcd(a,b))
+a=min(a,b)
+i=a
+while i:
+    if a%i==0 and b%i==0:
+        print(i)
+        break
+    i-=1
