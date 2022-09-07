df8ffffc1a834044b1ccccc590d70b1346b5bd43
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-09-07 14:00:36 +0530

    a,b=map(int,input().split()) m=max(a,b) while(1):     if(m%a==0 and m%b==0):         print(m)         break     m+=1

--- LCM.py
+++ LCM.py
@@ -1,13 +1,7 @@
-def lcm(x,y):
-    if(x>y):
-        greater=x
-    else:
-        greater=y
-    while(True):
-        if((greater%x==0)and (greater%y==0)):
-            lcm=greater
-            break
-        greater+=1
-    return lcm
 a,b=map(int,input().split())
-print(lcm(a,b))
+m=max(a,b)
+while(1):
+    if(m%a==0 and m%b==0):
+        print(m)
+        break
+    m+=1
