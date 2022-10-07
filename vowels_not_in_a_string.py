4ad749fc0443c6f4bede1bf095189149c07b6d92
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-07 11:43:04 +0530

    s=input() s=s.lower() c=0 if s.count('a')==0:     c+=1     print('a',end=' ') if s.count("e")==0:     c+=1     print('e',end=' ') if s.count("i")==0:     c+=1     print('i',end=' ') if s.count("o")==0:     c+=1     print('o',end=' ') if s.count("u")==0:     c+=1     print('u',end=' ') if c==0:     print("0")

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,20 @@
+s=input()
+s=s.lower()
+c=0
+if s.count('a')==0:
+    c+=1
+    print('a',end=' ')
+if s.count("e")==0:
+    c+=1
+    print('e',end=' ')
+if s.count("i")==0:
+    c+=1
+    print('i',end=' ')
+if s.count("o")==0:
+    c+=1
+    print('o',end=' ')
+if s.count("u")==0:
+    c+=1
+    print('u',end=' ')
+if c==0:
+    print("0")
