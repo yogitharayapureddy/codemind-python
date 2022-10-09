47c67ee72257251805b2bcf0ab6a797fb5ae6245
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-09 21:34:49 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s='' c=0 for i in s1:     if i not in s2:         if i not in s:             if i==" ":                 continue             s+=i             c+=1 for i in s2:     if i not in s1:         if i not in s:             if i==" ":                 continue             s+=i             c+=1 print(c)

---
+++ Non_common_characters.py
@@ -0,0 +1,21 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+s=''
+c=0
+for i in s1:
+    if i not in s2:
+        if i not in s:
+            if i==" ":
+                continue
+            s+=i
+            c+=1
+for i in s2:
+    if i not in s1:
+        if i not in s:
+            if i==" ":
+                continue
+            s+=i
+            c+=1
+print(c)
