f2cbdb157537d524463fd5d77fc598aa88f8450c
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-13 19:47:04 +0530

    n=int(input()) a=list(map(int,input().split())) temp=[] c=0 for element in a:     if(element not in temp):         temp.append(element)     c=0     for element in temp:         if element%2!=0:             c+=1 print(c)

--- odd_unique_elements_count.py
+++ odd_unique_elements_count.py
@@ -2,11 +2,11 @@ n=int(input())
 a=list(map(int,input().split()))
 temp=[]
 c=0
-for e in a:
-    if(e not in temp):
-        temp.append(e)
+for element in a:
+    if(element not in temp):  
+        temp.append(element) 
     c=0
-    for e in temp:
-        if e%2!=0:
+    for element in temp:
+        if element%2!=0:
             c+=1
 print(c)
