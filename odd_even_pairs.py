8ff9c811a54e1262bf4c1b499abeeb265614e784
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-13 19:27:21 +0530

    n=int(input()) l=list(map(int,input().split())) e=[] o=[] for i in l:     if i%2==0:         e.append(i)     else:         o.append(i) i,j=0,0 l1=[] while(i<len(e) and j<len(o)):     l1.append(o[j])     j+=1     l1.append(e[i])     i+=1 while(j<len(o)):     l1.append(o[j])     j+=1 while(i<len(e)):     l1.append(e[i])     i+=1 if len(l1)%2!=0:     l1.append("0") print(*l1)

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -1,27 +1,25 @@
 n=int(input())
-a=list(map(int,input().split()))
+l=list(map(int,input().split()))
 e=[]
 o=[]
-for i in range(len(a)):
-    if(a[i]%2==0):
-        e.append(a[i])
+for i in l:
+    if i%2==0:
+        e.append(i)
     else:
-        o.append(a[i])
-if len(o)>len(e):
-    for i in range(len(o)-len(e)):
-        e.append(0)
-elif len(e)>len(o):
-    for i in range(len(e)-len(o)):
-        o.append(0)
-d=[]
-for i in range(len(e)):
-    d.append(o[i])
-    d.append(e[i])
-for i in d:
-    if(i==0):
-        d.remove(0)
-if(len(d)%2==0):
-    print(*d)
-elif(len(d)%2==1):
-    d.append(0)
-    print(*d)
+        o.append(i)
+i,j=0,0
+l1=[]
+while(i<len(e) and j<len(o)):
+    l1.append(o[j])
+    j+=1
+    l1.append(e[i])
+    i+=1
+while(j<len(o)):
+    l1.append(o[j])
+    j+=1
+while(i<len(e)):
+    l1.append(e[i])
+    i+=1
+if len(l1)%2!=0:
+    l1.append("0")
+print(*l1)
