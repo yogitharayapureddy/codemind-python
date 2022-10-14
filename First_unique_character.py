63be93d579e3e1aee07696b5eebf50a1107b9db7
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-14 19:59:28 +0530

    class Solution(object):    def firstUniqChar(self, s):       frequency = {}       for i in s:          if i not in frequency:             frequency[i] = 1          else:             frequency[i] +=1       for i in range(len(s)):          if frequency[s[i]] == 1:             return s[i]       return -1 s=input('') ob1 = Solution() print(ob1.firstUniqChar(s))

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,15 +1,15 @@
-class solution(object):
-    def first(self,s):
-        f={}
-        for i in s:
-            if i not in f:
-                f[i]=1
-            else:
-                f[i]+=1
-        for i in range(len(s)):
-            if f[s[i]]==1:
-                return s[i]
-        return -1
+class Solution(object):
+   def firstUniqChar(self, s):
+      frequency = {}
+      for i in s:
+         if i not in frequency:
+            frequency[i] = 1
+         else:
+            frequency[i] +=1
+      for i in range(len(s)):
+         if frequency[s[i]] == 1:
+            return s[i]
+      return -1
 s=input('')
-ab1=solution()
-print(ab1.first(s))
+ob1 = Solution()
+print(ob1.firstUniqChar(s))
