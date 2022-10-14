5f64e8ac278227c579eef5a25601136333462d62
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-14 19:38:17 +0530

    string=input() vowels=0 l='' for i in string:       if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):           vowels+=1           if i not in l:               l+=i if vowels==0:     print("-1") else:     for j in l:         print(j,end=' ')

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,13 +1,13 @@
 string=input()
-v=0
+vowels=0
 l=''
 for i in string:
-    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
-        v+=1
-        if i not in l:
-            l+=i
-if v==0:
+      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+          vowels+=1
+          if i not in l:
+              l+=i
+if vowels==0:
     print("-1")
 else:
     for j in l:
-        print(j,end=" ")
+        print(j,end=' ')
