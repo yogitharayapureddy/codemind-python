1d5f4996f734f0d3519a6cad449d93685467e049
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-14 20:29:43 +0530

    s=input() s=s.lower() c=0 if s.count('a')==0:     c+=1     print('a',end=' ') if s.count('e')==0:     c+=1     print('e',end=' ') if s.count('i')==0:     c+=1     print('i',end=' ') if s.count('o')==0:     c+=1     print('o',end=' ') if s.count('u')==0:     c+=1     print('u',end=' ') if c==0:     print("0")

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -4,16 +4,16 @@ c=0
 if s.count('a')==0:
     c+=1
     print('a',end=' ')
-if s.count("e")==0:
+if s.count('e')==0:
     c+=1
     print('e',end=' ')
-if s.count("i")==0:
+if s.count('i')==0:
     c+=1
     print('i',end=' ')
-if s.count("o")==0:
+if s.count('o')==0:
     c+=1
     print('o',end=' ')
-if s.count("u")==0:
+if s.count('u')==0:
     c+=1
     print('u',end=' ')
 if c==0:
