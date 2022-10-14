c0112fd38f6227a4ee66559a5b0214b321a6ce86
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-14 20:24:20 +0530

    s=input() s=s.lower() l='' v='' c=0 for i in s:     if i not in l:         if i==' ':             continue         l+=i         c+=1 r=sorted(l) for i in r:     if i==' ':         continue     else:         v+=i print(v)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,11 +1,15 @@
 s=input()
 s=s.lower()
-a=''
+l=''
 v=''
+c=0
 for i in s:
-    if s.count(i)==1:
-        a+=i
-r=sorted(a)
+    if i not in l:
+        if i==' ':
+            continue
+        l+=i
+        c+=1
+r=sorted(l)
 for i in r:
     if i==' ':
         continue
