4d3c0732e226fc689803d1388e6edccf15c2db5f
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 22:14:09 +0530

    def check_isogram(str1):     return len(str1) == len(set(str1.lower()))
    print(check_isogram(input('')))

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,10 +1,4 @@
-i=input()
-i=list(i)
-i.sort()
-s=set(i)
-s=list(s)
-s.sort()
-if i==s:
-    print(True)
-else:
-    print(False)
+def check_isogram(str1):
+    return len(str1) == len(set(str1.lower()))
+
+print(check_isogram(input('')))
