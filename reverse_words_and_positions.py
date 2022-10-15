547f6863029a59ff6d0988c2b4d1d993c2d6da8e
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 22:17:28 +0530

    def reverseWordSentence(Sentence):     words = Sentence.split(" ")     newWords = [word[::-1] for word in words]     newSentence = " ".join(newWords)     return newSentence Sentence = input() string=reverseWordSentence(Sentence) s = string.split()[::-1] l = [] for i in s:     l.append(i) print(" ".join(l))

--- reverse_words_and_positions.py
+++ reverse_words_and_positions.py
@@ -1,4 +1,12 @@
-n=input().split()
-n=n[::-1]
-for i in n:
-    print(i[::-1],end=" ")
+def reverseWordSentence(Sentence):
+    words = Sentence.split(" ")
+    newWords = [word[::-1] for word in words]
+    newSentence = " ".join(newWords)
+    return newSentence
+Sentence = input()
+string=reverseWordSentence(Sentence)
+s = string.split()[::-1]
+l = []
+for i in s:
+    l.append(i)
+print(" ".join(l))
