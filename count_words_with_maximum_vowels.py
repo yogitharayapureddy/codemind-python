802435680b28df0a6890b847bb17b101ade2d70d
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 21:57:36 +0530

    s =input() s=s.lower() l=[] count=0 words = s.split() vowels = ['a','e','i','o','u'] for word in words:     c = 0     for i in range(0,len(word)):         if word[i] in vowels:             c+=1     l.append(c) b=max(l) for j in l:     if j==b:         count+=1 print(count)

--- count_words_with_maximum_vowels.py
+++ count_words_with_maximum_vowels.py
@@ -1,14 +1,14 @@
-s=input()
+s =input()
 s=s.lower()
 l=[]
 count=0
-words=s.split()
-vowels=['a','e','i','o','u']
-for word in words:
-    c=0
+words = s.split() 
+vowels = ['a','e','i','o','u'] 
+for word in words: 
+    c = 0
     for i in range(0,len(word)):
         if word[i] in vowels:
-            c+=1
+            c+=1 
     l.append(c)
 b=max(l)
 for j in l:
