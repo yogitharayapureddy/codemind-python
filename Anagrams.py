b7a9affd20250c7879954d3617674dbd6eeeb8e4
yogitha rayapureddy <97437490+yogitharayapureddy@users.noreply.github.com>
2022-10-15 22:55:54 +0530

    str1=input("") str2=input("") str1=str1.lower() str2=str2.lower() if(len(str1)==len(str2)):     sortedstr1=sorted(str1)     sortedstr2=sorted(str2)     if(sortedstr1==sortedstr2):         print("True")     else:         print("False") else:     print("False")

--- Anagrams.py
+++ Anagrams.py
@@ -1,6 +1,14 @@
-a=input().lower()
-b=input().lower()
-if (sorted(a)==sorted(b)):
-    print("True")
+str1=input("")
+str2=input("")
+str1=str1.lower()
+str2=str2.lower()
+if(len(str1)==len(str2)):
+    sortedstr1=sorted(str1)
+    sortedstr2=sorted(str2)
+    if(sortedstr1==sortedstr2):
+        print("True")
+    else:
+        print("False")
 else:
-    print(False)
+    print("False")
+    
